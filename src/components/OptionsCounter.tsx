b6b83e1ca6b782ca5edcd50da8f3b25d796b872c
DeviousLab <deviouslab@gmail.com>
2023-11-28 01:11:44 +0400

    feat: implemented animation to score count
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/OptionsCounter.tsx
+++ src/components/OptionsCounter.tsx
@@ -1,5 +1,7 @@
 import { Card } from '@/components/ui/card';
 import { CheckCircle2, XCircle } from 'lucide-react';
+import { motion } from 'framer-motion';
+
 import { Separator } from '@/components/ui/separator';
 
 type Props = {
@@ -11,14 +13,45 @@ const MCQCounter = ({ correct_answers, wrong_answers }: Props) => {
 	return (
 		<Card className='flex flex-row items-center justify-center p-2'>
 			<CheckCircle2 color='green' size={30} />
-			<span className='mx-3 text-2xl text-[green]'>{correct_answers}</span>
+			<motion.span
+				key={`correct_${correct_answers}`}
+				variants={variants}
+				animate={'show'}
+				initial='hide'
+				className='mx-3 text-2xl text-[green]'
+			>
+				{correct_answers}
+			</motion.span>
 
 			<Separator orientation='vertical' />
 
-			<span className='mx-3 text-2xl text-[red]'>{wrong_answers}</span>
+			<motion.span
+				key={`wrong_${wrong_answers}`}
+				variants={variants}
+				animate={'show'}
+				initial='hide'
+				className='mx-3 text-2xl text-[red]'
+			>
+				{wrong_answers}
+			</motion.span>
 			<XCircle color='red' size={30} />
 		</Card>
 	);
+};
+
+export const variants = {
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			ease: 'easeOut',
+			duration: 0.3,
+		},
+	},
+	hide: {
+		y: -20,
+		opacity: 0,
+	},
 };
 
 export default MCQCounter;
