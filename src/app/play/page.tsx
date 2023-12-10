b94708eb97f499e3af4d6697951c3a9a08f91bde
DeviousLab <deviouslab@gmail.com>
2023-12-11 03:25:01 +0400

    fix: migrated metadata to work on social share
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
 import Options from '@/components/Options';
-
+import { useTimerStore } from '@/store/zustand';
+import { useEffect } from 'react';
 export default function Play() {
-
+	const { setTime } = useTimerStore();
+	useEffect(() => {
+		setTime(new Date());
+	}, [setTime]);
 	return (
 		<main className='flex grow items-center justify-center'>
 			<Options />
